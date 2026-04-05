e5872446f3938adeef5fd5d52e0dd6b4f2be8822
quietinvestor <4750936+quietinvestor@users.noreply.github.com>
2026-04-05 11:03:20 +0200

    Remove telescope file explorer

--- config/lua/plugins/telescope.lua
+++ config/lua/plugins/telescope.lua
@@ -7,15 +7,16 @@ return {
 				"nvim-telescope/telescope-fzf-native.nvim",
 				build = "make",
 			},
-			"nvim-telescope/telescope-file-browser.nvim",
 		},
 		opts = {
 			defaults = {
 				path_display = { "smart" },
 			},
-			extensions = {
-				file_browser = {
-					hijack_netrw = true,
+			pickers = {
+				buffers = {
+					initial_mode = "normal",
+				},
+				find_files = {
 					initial_mode = "normal",
 				},
 			},
@@ -25,16 +26,10 @@ return {
 
 			telescope.setup(opts)
 			telescope.load_extension("fzf")
-			telescope.load_extension("file_browser")
 		end,
 		keys = {
 			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
 			{ "<leader>fF", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find files (hidden)" },
-			{
-				"<leader>fe",
-				"<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>",
-				desc = "File browser",
-			},
 			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
 			{ "<leader>fs", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search current file" },
 			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
